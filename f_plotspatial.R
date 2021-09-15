3b41d16a8eb53ea4f0fc28113ecec567adaeb50b
Camille Crapart <c.m.crapart@kjemi.uio.no>
2021-09-15 17:34:49 +0200

    up to date commit

--- f_plotspatial.R
+++ f_plotspatial.R
@@ -1,7 +1,7 @@
 # Convinience function for plotting fennoscandian waterchemistry data
 # and results using ggplot2 
 
-f_plotspatial <- function(data,var,plottitle,col){
+f_plotspatial <- function(data,var,plottitle,lim = c(min(var),max(var)),midpoint = 0){
   require(ggplot2)
   pop <- ggplot(data) + 
     aes(Longitude,Latitude,var) + 
@@ -14,7 +14,7 @@ f_plotspatial <- function(data,var,plottitle,col){
           panel.grid.major = element_blank()) + 
     scale_x_discrete(breaks=NULL) + 
     scale_y_discrete(breaks=NULL) + 
-    scale_colour_gradient2(low="#4575B4",mid="#FFFFBF",high = "#D73027",midpoint = 0) + 
+    scale_colour_gradient2(low="#4575B4",mid="#FFFFBF",high = "#D73027",midpoint = midpoint,limits=lim) + 
     borders(database = "world", regions = c("Norway","Sweden","Finland"), 
             fill = NA, colour = "grey50",xlim=c(0,35),ylim=c(55,73)) + 
     xlab("") + ylab("") +
